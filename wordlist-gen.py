6409a4dc91e009d400ac9149fc3626ce344729e1
ulif <uli@gnufix.de>
2015-12-22 17:20:28 +0100

    Add first piece of code.

---
+++ wordlist-gen.py
@@ -0,0 +1,32 @@
+import argparse
+import sys
+
+
+def get_cmdline_args(args=None):
+    """Handle commandline options.
+    """
+    parser = argparse.ArgumentParser(description="Create a wordlist")
+    parser.add_argument(
+        '-l', '--len', default=8192, type=int,
+        help='desired length of generated wordlist. Default: 8192')
+    parser.add_argument(
+        'dictfile', nargs='+', metavar='DICTFILE',
+        type=argparse.FileType('rb'),
+        help=("Dictionary file to read possible terms from. "
+              "Multiple allowed. `-' will read from stdin."),
+    )
+    parser.add_argument(
+        '-v', '--verbose', action='store_true',
+        help='be verbose.')
+    return parser.parse_args(args)
+
+
+def filter_list():
+    with open(INFILE, "r") as fd:
+        for line in fd.readlines():
+            print(line)
+
+
+if __name__ == "__main__":
+    args = get_cmdline_args()
+    print(args)
