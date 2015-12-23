28c25daeab320eb72734b7652a33d98731014e8c
ulif <uli@gnufix.de>
2015-12-24 00:39:22 +0100

    Further simplifications of tests.

--- test.py
+++ test.py
@@ -24,20 +24,18 @@ class TestArgParser(object):
         out, err = capfd.readouterr()
         assert err == ""
 
-    def test_dict_file_required(self, monkeypatch, capfd):
+    def test_dict_file_required(self, capfd):
         # we require at least one argument, a dictionary file
-        monkeypatch.setattr(sys, "argv", ["scriptname", ])
         with pytest.raises(SystemExit) as why:
-            get_cmdline_args()
+            get_cmdline_args(None)
         assert why.value.args[0] == 2
         out, err = capfd.readouterr()
         assert "the following arguments are required" in err
 
     def test_dict_file_must_exist(self, monkeypatch, capfd):
         # we require at least one argument, a dictionary file
-        monkeypatch.setattr(sys, "argv", ["scriptname", "foobar"])
         with pytest.raises(SystemExit):
-            get_cmdline_args()
+            get_cmdline_args(["foobar", ])
         out, err = capfd.readouterr()
         assert "No such file or directory: 'foobar'" in err
 
