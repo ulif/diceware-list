25ee21fce5416f5d2d33949030ab757a2bc74c10
ulif <uli@gnufix.de>
2015-12-25 21:32:09 +0100

    Aggregate filtered_by_len tests in own class.

--- test.py
+++ test.py
@@ -70,14 +70,16 @@ class TestArgParser(object):
         assert result.len == 1024
 
 
-def test_filtered_by_len():
-    buf = StringIO(u"Line1\nLine12\n")
-    result = list(filtered_by_len(buf))
-    assert result == ["Line1", "Line12"]
+class TestFilteredByLen(object):
 
-def test_filtered_by_len_min_len():
-    # we can set minimal length of accepted terms
-    buf = StringIO(u"\n".join(["1", "12", "123", "1234"]))
-    assert list(filtered_by_len(buf)) == ["123", "1234"]
-    assert list(filtered_by_len(buf, min_len=2)) == ["12", "123", "1234"]
-    assert list(filtered_by_len(buf, min_len=4)) == ["1234", ]
+    def test_filtered_by_len():
+        buf = StringIO(u"Line1\nLine12\n")
+        result = list(filtered_by_len(buf))
+        assert result == ["Line1", "Line12"]
+
+    def test_filtered_by_len_min_len():
+        # we can set minimal length of accepted terms
+        buf = StringIO(u"\n".join(["1", "12", "123", "1234"]))
+        assert list(filtered_by_len(buf)) == ["123", "1234"]
+        assert list(filtered_by_len(buf, min_len=2)) == ["12", "123", "1234"]
+        assert list(filtered_by_len(buf, min_len=4)) == ["1234", ]
