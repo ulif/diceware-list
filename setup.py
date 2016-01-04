d9a2ef75d2aa6208ae92b039df25db4b9ac88ff7
ulif <uli@gnufix.de>
2016-01-04 23:30:10 +0100

    Officially support Python 2.7.

--- setup.py
+++ setup.py
@@ -43,6 +43,8 @@ setup(
             "License :: OSI Approved :: "
             "GNU General Public License v3 or later (GPLv3+)"),
         "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: Implementation :: CPython",
