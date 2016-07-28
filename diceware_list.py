127b6d00ecf2ac3487479a27667fdc4ecbab54d4
ulif <uli@gnufix.de>
2016-07-28 09:30:45 +0200

    Tell more about base10 conversion.

--- diceware_list.py
+++ diceware_list.py
@@ -70,6 +70,21 @@ def base10_to_n(num, base):
     """Turn base-10 integer `num` into base-`base` form.
 
     Returns a list of numbers representing digits in `base`.
+
+    For instance in base-2 we have only the digits ``0`` and
+    ``1``. Turning the base-10 integer ``5`` into a base-2 number
+    results in ``101`` or, as a list, in::
+
+        >>> base10_to_n(5, base=2)
+        [1, 0, 1]
+
+    The result list represents the single "digits" of a differently
+    based number. This holds also for 'digits' >= 10::
+
+        >>> base10_to_n(127, base=16)
+        [7, 15]
+
+    which in hexadecimal notation would normally read ``0x7F``.
     """
     result = []
     curr = num
