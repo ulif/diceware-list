0935d781689fe355ed94c0fb8ba5c68e3b353f0a
ulif <uli@gnufix.de>
2016-08-07 02:02:12 +0200

    Squash tests.
    The two tests squashed belong together.

--- tests/test_wordlistlib.py
+++ tests/test_wordlistlib.py
@@ -81,11 +81,6 @@ def test_min_width_iter_shuffle_max_widths_values(monkeypatch):
     monkeypatch.setattr(random, "shuffle", lambda x: x.reverse())
     assert list(min_width_iter(
         ["a", "aa", "bb"], 2, shuffle_max_width=True)) == ["a", "bb"]
-
-
-def test_min_width_iter_shuffling_ignores_too_longs(monkeypatch):
-    # we determine the max_width correctly
-    monkeypatch.setattr(random, "shuffle", lambda x: x.reverse())
     assert list(min_width_iter(
         ["bbb", "aa", "a"], 2, shuffle_max_width=True)) == ["a", "aa"]
     assert list(min_width_iter(
