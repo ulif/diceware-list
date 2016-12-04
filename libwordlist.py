c3f83ea82bea336f9b0852bd7d8396f36d12ca0d
ulif <uli@gnufix.de>
2016-12-04 14:54:59 +0100

    Make `prefer_short` option work.

--- libwordlist.py
+++ libwordlist.py
@@ -341,8 +341,14 @@ def strip_matching_prefixes(iterable, is_sorted=False, prefer_short=True):
         elems.sort()
     last = None
     for item in elems:
-        if last is None:
-            yield item
-        if last and not item.startswith(last):
-            yield item
+        if prefer_short:
+            if last is None:
+                yield item
+            elif not item.startswith(last):
+                yield item
+        else:
+            if last and not item.startswith(last):
+                yield last
         last = item
+    if last and not prefer_short:
+        yield last
