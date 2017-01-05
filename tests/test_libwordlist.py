4d31135b4a197d38b25033be5794e0e4b6b97f92
ulif <uli@gnufix.de>
2017-01-05 18:01:59 +0100

    Also test prefix finder with some edge cases.

--- tests/test_libwordlist.py
+++ tests/test_libwordlist.py
@@ -312,6 +312,8 @@ class TestTermIterator(object):
 
     def test_get_prefixes(self):
         # we can create tree-like nested lists of prefixed lists of strings
+        assert get_prefixes([]) == []
+        assert get_prefixes(["a"]) == [["a"]]
         assert get_prefixes(["a", "b"]) == [["a"], ["b"]]
         assert get_prefixes(["a", "ab"]) == [["a", ["ab"]]]
         assert get_prefixes(["a", "aa", "b"]) == [["a", ["aa"]], ["b"]]
