a380b56621ffb52937753ae4c0cc88e00a9b87b7
ulif <uli@gnufix.de>
2017-01-21 03:14:57 +0100

    Test flattening complex prefix tree.

--- tests/test_libwordlist.py
+++ tests/test_libwordlist.py
@@ -348,3 +348,6 @@ class TestTermIterator(object):
         assert flatten_prefix_tree([["a"], ["b"]]) == ["a", "b"]
         assert flatten_prefix_tree([["a", ["ab"]]]) == ["a"]
         assert flatten_prefix_tree([["a", ["ab"]]], prefer_short=False) == ["ab"]
+        assert flatten_prefix_tree(
+            [['a', ['aa', ['aaa']], ['ab'], ['ac']]], prefer_short=False) == [
+                'aaa', 'ab', 'ac']
