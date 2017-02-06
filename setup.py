db17207e78dc8f70943ee5fb5cfed5fc76487a23
ulif <uli@gnufix.de>
2017-02-06 22:46:19 +0100

    Declare pypy compatibility.

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
     ],
     include_package_data=True,
     zip_safe=False,
