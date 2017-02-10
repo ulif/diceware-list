b10829919813f2897b6d322877982b6338b751b1
ulif <uli@gnufix.de>
2017-02-11 00:14:11 +0100

    Move pytest fixtures into conftest.py.

---
+++ tests/conftest.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+#  diceware_list -- generate wordlists for diceware
+#  Copyright (C) 2017  Uli Fouquet
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""py.test config for `diceware-list` modules.
+"""
+import pytest
+
+@pytest.fixture
+def dictfile(request, tmpdir):
+    """py.test fixture providing a dictfile.
+
+    The returned file is a py.local instance.
+
+    The entries in here are mainy like ``zzz0``...``zzz8192``.
+    This is not a prefix code.
+    """
+    dictfile = tmpdir / "dictfile.txt"
+    contents = "\n".join(["zzz%s" % x for x in range(8192)])
+    dictfile.write("foo\nbar\n" + contents)
+    return dictfile
+
+
+@pytest.fixture
+def dictfile_ext(request, tmpdir):
+    """py.test fixture providing a dictfile which is prefix code.
+
+    The returned file is a py.local instance. Different from the other
+    `dictfile`, this one is nearly a prefix code, except the both entries
+    ``zzz0000`` and ``zzz00000``.
+    """
+    dictfile = tmpdir / "dictfile.txt"
+    contents = "\n".join(["zzz%04d" % x for x in range(8192)])
+    dictfile.write("a\nbb\nbbb\nc\n" + contents)
+    return dictfile
