1151919edad47cceca14e6fd7cdd0310a1921394
ulif <uli@gnufix.de>
2017-10-01 23:58:16 +0200

    Add func to parse wordlists.

--- diceware_list/libwordlist.py
+++ diceware_list/libwordlist.py
@@ -513,3 +513,9 @@ class AndroidWordList(object):
         line = line.decode('utf-8')
         data = [tuple(x.split('=')) for x in line.split(',')]
         return dict(data)
+
+    def parse_lines(self):
+        for line in self._data.split(b'\n'):
+            line = line.decode('utf-8')
+            data = [tuple(x.split('=')) for x in line.split(',')]
+            yield data
