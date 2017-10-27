9342a8dcd2b841d57adb88f23655641fb4f1add4
ulif <uli@gnufix.de>
2017-10-27 13:06:04 +0200

    Add tests for new wldownload module.

---
+++ tests/test_wldownload.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#  diceware_list -- generate wordlists for diceware
+#  Copyright (C) 2017.  Uli Fouquet
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Tests for wldownload module
+from __future__ import unicode_literals
+import pytest
+import sys
+from diceware_list.wldownload import (
+        download_wordlist, get_cmdline_args, main
+        )
+
+
+class TestMain(object):
+
+    def test_can_get_help(self, monkeypatch, capfd, home_dir):
+        # we can get help
+        monkeypatch.setattr(sys, "argv", ["scriptname", "--help"])
+        with pytest.raises(SystemExit):
+            main()
+        out, err = capfd.readouterr()
+        assert "show this help message" in out
