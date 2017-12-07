02da03af743d12fdb15d8db1dedc841cdafd7001
ulif <uli@gnufix.de>
2017-12-07 03:57:17 +0100

    Test `lang` attribute.

--- tests/test_libwordlist.py
+++ tests/test_libwordlist.py
@@ -380,6 +380,7 @@ class TestAndroidWordlist(object):
         assert hasattr(wl, "base_url")
         assert hasattr(wl, "path")
         assert hasattr(wl, "gz_data")
+        assert hasattr(wl, "lang")
 
     def test_init_path(self, local_android_dir):
         # we can pass in a path to an unencoded file (no base64).
