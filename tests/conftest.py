6bbf91167058175bc68f1623fec2e042cdc0f9e9
ulif <uli@gnufix.de>
2017-12-13 12:01:44 +0100

    Add a test fixture to restore sys.stdin.

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,6 +22,7 @@ import logging
 import os
 import pytest
 import shutil
+import sys
 
 
 @pytest.fixture
@@ -134,3 +135,16 @@ def teardown_loggers():
     for handler in logger.handlers:
         logger.removeHandler(handler)
         logger.setLevel(logging.NOTSET)
+
+
+@pytest.fixture(scope="function")
+def argv_handler(request):
+    """This fixture restores sys.argv and sys.stdin after tests.
+    """
+    _argv_stored = sys.argv
+    _stdin_stored = sys.stdin
+
+    def teardown():
+        sys.argv = _argv_stored
+        sys.stdin = _stdin_stored
+    request.addfinalizer(teardown)
