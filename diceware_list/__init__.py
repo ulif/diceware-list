f0ae6e775b09903103a9aa373a3a588c11e99ace
ulif <uli@gnufix.de>
2020-02-03 23:38:33 +0100

    Update yearnumber.
    Do not display every release year but only the first year.

--- diceware_list/__init__.py
+++ diceware_list/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #  diceware_list -- generate wordlists for diceware
-#  Copyright (C) 2016-2018  Uli Fouquet
+#  Copyright (C) 2016 Uli Fouquet
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
